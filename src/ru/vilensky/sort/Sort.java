9ea55ddcf05442c134e038b0eb0b82e6ac5ca794
sergei vilensky <sergey3282@gmail.com>
2023-01-03 21:39:26 +0200

    sort added

---
+++ src/ru/vilensky/sort/Sort.java
@@ -0,0 +1,36 @@
+package ru.vilensky.sort;
+//TODO: merge, quick, insert, heap, maybe bucket
+public class Sort <E extends Comparable<E>>{
+    public E[] merge(E[] array){
+        int len = array.length;
+        return null;
+    }
+
+    public E[] quick(E[] array){
+        int len = array.length;
+        return null;
+    }
+
+    public E[] bubble(E[] array){
+        int len = array.length;
+        for(int i = 0; i < len - 1; i++){
+            for(int j = 0; j < len - i - 1; j++){
+                if(array[j].compareTo(array[j + 1]) > 0){
+                    swap(array, j, j + 1);
+                }
+            }
+        }
+        return array;
+    }
+
+    public E[] insert(E[] array){
+        int len = array.length;
+        return null;
+    }
+
+    private void swap(E[] array, int i, int j){
+        E t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+}
