723a615870cc2540c6df84f5629dfaa4cd97a3d6
Sergei Vilensky <sergey3282@gmail.com>
2023-01-11 10:06:06 -0200

    quicksort completed

--- src/ru/vilensky/sort/Main.java
+++ src/ru/vilensky/sort/Main.java
@@ -2,16 +2,15 @@ package ru.vilensky.sort;
 //TODO: test class, remove
 public class Main {
     public static void main(String[] args) {
-        Integer[] A = {12, 3, 5 ,50, 21, 24, 11, 5, 17, 8, 1, 1};
+        Integer[] a = {12, 3, 5 ,50, 21, 24, 11, 5, 17, 8, 1, 1};
         String[] s = {"AAA", "hgyt", "dggf", "aa", "uty", "nn", "mnm"};
-        Sort<String> stringSort = new Sort<>();
-        Sort<Integer> sort = new Sort<>();
-        Integer[] B = sort.bubble(A);
-        String[] S = stringSort.bubble(s);
-        for(Integer i: B){
+        Sort srt = new Sort();
+        srt.bubble(a);
+        srt.quick(s);
+        for(Integer i: a){
             System.out.print(i+" ");
         }
-        for(String i: S){
+        for(String i: s){
             System.out.print(i+" ");
         }
     }
--- src/ru/vilensky/sort/Sort.java
+++ src/ru/vilensky/sort/Sort.java
@@ -1,17 +1,38 @@
 package ru.vilensky.sort;
-//TODO: merge, quick, insert, heap, maybe bucket
-public class Sort <E extends Comparable<E>>{
-    public E[] merge(E[] array){
+//TODO: merge, insert, maybe bucket
+public class Sort {
+    public <E extends Comparable<E>> void merge(E[] array){
         int len = array.length;
-        return null;
     }
 
-    public E[] quick(E[] array){
-        int len = array.length;
-        return null;
+    public <E extends Comparable<E>> void quick(E[] array){
+		quick(array, 0, array.length - 1);
     }
 
-    public E[] bubble(E[] array){
+	private <E extends Comparable<E>> void quick(E[] array, int left, int right){
+		if(left < right){
+			int pivotIndex = partition(array, left, right);
+			quick(array, left, pivotIndex - 1);
+			quick(array, pivotIndex + 1, right);
+		}
+	}
+
+	//choosing rightmost element as pivot
+	private <E extends Comparable<E>> int partition(E[] array, int left, int right){
+		E pivot = array[right];
+		int pivotWall = left- 1;
+		for(int j = left; j < right; j++){
+			if(array[j].compareTo(pivot) <= 0){
+				pivotWall++;
+				swap(array, pivotWall, j);
+			}
+		}
+		swap(array, pivotWall + 1, right);
+
+		return pivotWall + 1;
+	}
+
+    public <E extends Comparable<E>> void bubble(E[] array){
         int len = array.length;
         for(int i = 0; i < len - 1; i++){
             for(int j = 0; j < len - i - 1; j++){
@@ -20,15 +41,13 @@ public class Sort <E extends Comparable<E>>{
                 }
             }
         }
-        return array;
     }
 
-    public E[] insertion(E[] array){
+    public <E extends Comparable<E>> void insertion(E[] array){
         int len = array.length;
-        return null;
     }
 
-    private void swap(E[] array, int i, int j){
+    private <E extends Comparable<E>> void swap(E[] array, int i, int j){
         E t = array[i];
         array[i] = array[j];
         array[j] = t;
