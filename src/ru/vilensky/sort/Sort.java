268ef89a3650b507e4d1418cce68c2af63a3cde5
Sergei Vilensky <sergey3282@gmail.com>
2023-01-19 17:10:02 -0200

    merge finished

--- src/ru/vilensky/sort/Sort.java
+++ src/ru/vilensky/sort/Sort.java
@@ -1,9 +1,63 @@
 package ru.vilensky.sort;
-//TODO: merge, insert, maybe bucket
+//TODO:  insert, maybe bucket
 public class Sort {
     public <E extends Comparable<E>> void merge(E[] array){
-        int len = array.length;
+		merge(array, 0, array.length - 1);
     }
+
+	private <E extends Comparable<E>> void merge(E[] array, int left, int right){
+		if(left < right){
+			int mid = (left + right)/2;
+			merge(array, left, mid);
+			merge(array, mid + 1, right);
+			mergeSort(array, left, mid, right);
+		}
+	}
+
+	private <E extends Comparable<E>> void mergeSort(E[] array, int left, int mid, int right){
+		int lenLeft = mid - left + 1;
+		int lenRight = right - mid;
+		
+		//temp arrays
+		E[] leftArr = (E[])new Object[lenLeft];
+		E[] rightArr = (E[])new Object[lenRight];
+		
+		//fill temp arrays
+		for(int i = 0; i<lenLeft; i++){
+			leftArr[i] = array[left + i];
+		}
+		
+		for(int i = 0; i<lenRight; i++){
+			rightArr[i] = array[mid + i + 1];
+		}
+
+		int i = 0;
+		int j = 0;
+		int k = left;
+
+		while (i < lenLeft && j < lenRight){    
+        	if(leftArr[i].compareTo(rightArr[j])<=0){    
+            	array[k] = leftArr[i];    
+            	i++;    
+        	}    
+        	else{    
+            	array[k] = rightArr[j];    
+            	j++;    
+        	}    
+			k++;    
+		}    
+		while (i<lenLeft){    
+        	array[k] = leftArr[i];    
+			i++;    
+			k++;    
+		}    
+      
+		while (j<lenRight){    
+			array[k] = rightArr[j];    
+			j++;    
+			k++;    
+		}
+	}
 
     public <E extends Comparable<E>> void quick(E[] array){
 		quick(array, 0, array.length - 1);
