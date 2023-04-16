a6f50bd269c3a75fcec170208359f74b104de943
sergey <sergey3282@gmail.com>
2023-04-16 12:59:18 +0300

    genart example added

--- src/ru/vilensky/generative_art/Consts.java
+++ src/ru/vilensky/generative_art/Consts.java
@@ -3,7 +3,8 @@ package ru.vilensky.generative_art;
 public interface Consts {
     final int WIDTH = 1000;
     final int HEIGHT = 1000;
-    final int NUM_OF_RECTS = 10;
+    final int NUM_OF_RECTS = 8;
     final int RECT_SIZE =70;
     final int GAP_SIZE = 20;
+    final float LAMBDA = 1.4f;
 }
--- src/ru/vilensky/generative_art/GenArtBasic.java
+++ src/ru/vilensky/generative_art/GenArtBasic.java
@@ -18,29 +18,49 @@ public class GenArtBasic extends PApplet implements Consts{
 
     public void draw(){
         background(255);
+        stroke(120);
+        float[] quads = getRectArray();
+        for(int i = 0; i<quads.length - 1; i+=8){
+            quad(quads[i], quads[i+1],
+                 quads[i+2], quads[i+3],
+                 quads[i+4], quads[i+5],
+                 quads[i+6], quads[i+7]);
+        }
 
         //since draw() redrawing window infinitely, noLoop() changes this behavior
         noLoop();
     }
 
     //TODO: move constants to Const.java
-    public static PShape[] getRectArray(){
+    public static float[] getRectArray(){
         SecureRandom random = new SecureRandom();
-        int x1 = WIDTH - (RECT_SIZE*NUM_OF_RECTS + GAP_SIZE*(NUM_OF_RECTS - 1));
-        int y1 = x1;
-        PShape[] rectArray = new PShape[NUM_OF_RECTS*NUM_OF_RECTS];
+        float x1 = (WIDTH - (RECT_SIZE*NUM_OF_RECTS + GAP_SIZE*(NUM_OF_RECTS - 1)))/2;
+        float y1 = x1;
+        float[] rectArray = new float[NUM_OF_RECTS*NUM_OF_RECTS*8];
         int counter = 0;
         
         for(int i = 0; i < NUM_OF_RECTS; i++){
             for (int j = 0; j < NUM_OF_RECTS; j++) {
-                PShape pShape = 
+                rectArray[counter] = x1 + (random.nextFloat(2) - 1)*(i+j)*LAMBDA;
+                rectArray[counter+1] = y1 + (random.nextFloat(2) - 1)*(i+j)*LAMBDA;
+                rectArray[counter+2] = x1 + RECT_SIZE + (random.nextFloat(2) - 1)*(i+j)*LAMBDA;
+                rectArray[counter+3] = y1 + (random.nextFloat(2) - 1)*(i+j)*LAMBDA;
+                rectArray[counter+4] = x1 + RECT_SIZE + (random.nextFloat(2) - 1)*(i+j)*LAMBDA;
+                rectArray[counter+5] = y1 + RECT_SIZE + (random.nextFloat(2) - 1)*(i+j)*LAMBDA;
+                rectArray[counter+6] = x1 + (random.nextFloat(2) - 1)*(i+j)*LAMBDA;
+                rectArray[counter+7] = y1 + RECT_SIZE + (random.nextFloat(2) - 1)*(i+j)*LAMBDA;
+                counter+=8;
+                System.out.print(x1+" "+y1+"--");
+                x1 += RECT_SIZE + GAP_SIZE;
             }
+            x1 = (WIDTH - (RECT_SIZE*NUM_OF_RECTS + GAP_SIZE*(NUM_OF_RECTS - 1)))/2;
+            y1+=RECT_SIZE+GAP_SIZE;
         }
 
         return rectArray;
     }
 
-    public static PShape makeShape(color c, )
+    //public static PShape makeShape(color c, )
 
     public static void main(String[] args) {
         PApplet.main("ru.vilensky.generative_art.GenArtBasic");
